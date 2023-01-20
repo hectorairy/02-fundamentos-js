80ea199960d9b75db53d2036e5c06fdb52a69751
Héctor Airy <hello@hectorairy.com>
2023-01-19 19:44:21 -0600

    template strings

---
+++ template-strings.js
@@ -0,0 +1,16 @@
+const nickname = "Deadpool";
+
+const nombre = "Wade Winston";
+
+const normal = nickname + " " + nombre;
+
+const template = `${nickname} ${nombre}`;
+
+console.log(normal);
+console.log(template);
+console.log(normal === template);
+
+const html = `<h1>Hola</h1>
+<p>Mundo</p>`;
+
+console.log(html);
